30fae7b1d25e6fed90a8ba687aa999e1be6cd574
Arunas Slepikas <dijitle@gmail.com>
2020-03-12 19:50:52 -0500

    bugfix - get right calendar

--- src/Dijitle.Metra.Data/AllData.cs
+++ src/Dijitle.Metra.Data/AllData.cs
@@ -79,8 +79,8 @@ namespace Dijitle.Metra.Data
 
         public IEnumerable<Calendar> GetCurrentCalendars(DateTime date)
         {
-            return Calendars.Where(c => c.Value.start_date < date)
-                            .Where(c => c.Value.end_date.AddDays(1) >= date)
+            return Calendars.Where(c => c.Value.start_date <= date)
+                            .Where(c => c.Value.end_date.AddDays(1) > date)
                             .Where(c => c.Value.IsDay(date.DayOfWeek) == true)
                             .Where(c => c.Value.CalendarDates.All(cd => !(cd.exception_type == CalendarDate.Exception_Type.Removed && cd.date == date.Date)) ||
                                    c.Value.CalendarDates.Count == 0).Select(o => o.Value);
